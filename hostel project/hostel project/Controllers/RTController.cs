ca71da6b51b151fcb3b929072eda9f0ea9024449
Heena <heenairfan104@gmail.com>
2018-11-19 01:24:47 +0500

    Integrate RT and Student

---
+++ hostel project/hostel project/Controllers/RTController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hostel_project.Controllers
+{
+    public class RTController : Controller
+    {
+        // GET: RT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult OneDayMenu()
+        {
+            return View();
+        }
+
+        public ActionResult Requests()
+        {
+            return View();
+        }
+    }
+}
---
+++ hostel project/hostel project/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hostel_project.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddComplains()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDetails()
+        {
+            return View();
+        }
+
+        public ActionResult Calender()
+        {
+            return View();
+        }
+    }
+}
