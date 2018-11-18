95eccf3795eb446252cd596bca2c2a5d67240ef3
rahta attique <rahatattique@gmail.com>
2018-11-18 18:06:01 +0500

    integration of gui with visual

---
+++ hostel project/hostel project/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hostel_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Complains()
+        {
+            return View();
+        }
+        public ActionResult BillCalculation()
+        {
+            return View();
+        }
+    }
+}
---
+++ hostel project/hostel project/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hostel_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dinner()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult expenses()
+        {
+           
+
+            return View();
+        }
+        public ActionResult approvedrequest()
+        {
+          
+
+            return View();
+        }
+        public ActionResult newrequests()
+        {
+            
+
+            return View();
+        }
+        public ActionResult lunch()
+        {
+            
+
+            return View();
+        }
+    }
+}
